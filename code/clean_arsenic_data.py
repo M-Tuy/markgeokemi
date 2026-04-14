70ad7843a9bef608301367ff218fdef2144a452a
[Marius Tuyishime] <tuyishimemarius@gmail.com>
2026-04-14 14:49:46 +0200

    update arsenic_cean notbook, notbook

--- code/clean_arsenic_data.py
+++ code/clean_arsenic_data.py
@@ -1,16 +1,14 @@
 import geopandas as gpd
-markgeokemi_regional= gpd.read_file (
-    "C:/Projects/markgeokemi/raw_data/markgeokemi_regional.gpkg", 
-    layer= "moran_0063mm_hno3_icpms"
-)
-arsenic_icpms= markgeokemi_regional[
-    ["unikt_id", "geometry","ns", "ew", "prov_artal","provtyp", "as_ppm"]
-].copy()
-arsenic_icpms_clean= arsenic_icpms.dropna(
-    subset=["as_ppm", "ns", "ew", "geometry"]
-).copy() # remove NaN
+gpd.options.io_engine = "pyogrio"
+arsenic_icpms_clean = gpd.read_file(
+    "C:/Projects/markgeokemi/raw_data/markgeokemi_regional.gpkg",
+    layer = "moran_0063mm_hno3_icpms",
+    columns = ["unikt_id", "geometry", "ns", "ew", "prov_artal", "provtyp", "as_ppm"],
+    engine = "pyogrio",
+    use_arrow = True
+).dropna(subset=["as_ppm", "ns", "ew", "geometry"]).copy() # remove NaN
 arsenic_icpms_clean.to_file(
     "C:/Projects/markgeokemi/cleaned_data/arsenic_icpms_clean.gpkg", driver="GPKG"
 ) #save in cleaned data
 arsenic_icpms_clean = arsenic_icpms_clean.set_crs("EPSG:3006", allow_override=True)#assign CRS, update
-arsenic_icpms_clean.crs# 
+arsenic_icpms_clean.crs
